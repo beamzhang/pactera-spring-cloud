ec530f4f0abd911d7e080a96f97b49dd0707a4df
Administrator <Administrator@pactera>
2017-11-10 13:44:06 +0800

    使用fallback factory查看回退原因

--- pactera-consumer-account-feign-hystrix-fallback-factory/src/main/java/com/pactera/cloud/study/user/feign/UserFeignClient.java
+++ pactera-consumer-account-feign-hystrix-fallback-factory/src/main/java/com/pactera/cloud/study/user/feign/UserFeignClient.java
@@ -13,35 +13,35 @@ import feign.hystrix.FallbackFactory;
 
 @FeignClient(name = "pactera-provider-user", fallbackFactory = FeignClientFallbackFactory.class)
 public interface UserFeignClient {
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public User findById(@PathVariable("id") Long id);
 }
 
 /**
- * UserFeignClient的fallbackFactory类，该类需实现FallbackFactory接口，并覆写create方法
- * The fallback factory must produce instances of fallback classes that
- * implement the interface annotated by {@link FeignClient}.
+ * UserFeignClient的fallbackFactory类，该类需实现FallbackFactory接口，并覆写create方法 The
+ * fallback factory must produce instances of fallback classes that implement
+ * the interface annotated by {@link FeignClient}.
  * 
  */
 @Component
 class FeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
-  private static final Logger LOGGER = LoggerFactory.getLogger(FeignClientFallbackFactory.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(FeignClientFallbackFactory.class);
 
-  @Override
-  public UserFeignClient create(Throwable cause) {
-    return new UserFeignClient() {
-      @Override
-      public User findById(Long id) {
-        // 日志最好放在各个fallback方法中，而不要直接放在create方法中。
-        // 否则在引用启动时，就会打印该日志。
-        // 详见https://github.com/spring-cloud/spring-cloud-netflix/issues/1471
-        FeignClientFallbackFactory.LOGGER.info("fallback; reason was:", cause);
-        User user = new User();
-        user.setId(-1L);
-        user.setUsername("默认用户");
-        return user;
-      }
-    };
-  }
+	@Override
+	public UserFeignClient create(Throwable cause) {
+		return new UserFeignClient() {
+			@Override
+			public User findById(Long id) {
+				// 日志最好放在各个fallback方法中，而不要直接放在create方法中。
+				// 否则在引用启动时，就会打印该日志。
+				// 详见https://github.com/spring-cloud/spring-cloud-netflix/issues/1471
+				FeignClientFallbackFactory.LOGGER.info("fallback; reason was:", cause);
+				User user = new User();
+				user.setId(-1L);
+				user.setUsername("默认用户");
+				return user;
+			}
+		};
+	}
 }
